fb1802df5112bdafaea85e6dce7caf79b84d7f0f
varshini-shanmugakumar <varshu016@gmail.com>
2026-08-28 18:50:53 +0530

    Add auditing and indexing to risk assessments

--- transaction-service/src/main/java/com/varshini/sentinel/transaction/model/RiskAssessment.java
+++ transaction-service/src/main/java/com/varshini/sentinel/transaction/model/RiskAssessment.java
@@ -2,9 +2,11 @@ package com.varshini.sentinel.transaction.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,9 +16,15 @@ import java.util.UUID;
 public class RiskAssessment {
     @Id
     private String id;
-
+    @Indexed(unique = true)
     private UUID transactionId;
+
     private int riskScore;
     private RiskLevel  riskLevel;
     private List<String> reasons;
+
+    @CreatedDate
+    private Instant createdAt;
+    @LastModifiedDate
+    private Instant updatedAt;
 }
